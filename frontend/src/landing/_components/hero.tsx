01672e223f7cd1031bae1c0e84532d814a88dba5
Ankur123unsungDevloper <ankurdas1804@gmail.com>
2026-02-12 00:19:25 +0530

    new file:   .vscode/settings.json 	renamed:    frontend/src/landing/_components/heading.tsx -> frontend/src/landing/_components/hero.tsx 	modified:   frontend/src/landing/landingpage.tsx

--- frontend/src/landing/_components/hero.tsx
+++ frontend/src/landing/_components/hero.tsx
@@ -1,7 +1,7 @@
 import { Button } from "@/components/ui/button";
 import { MdOutlineKeyboardArrowRight } from "react-icons/md";
 
-export default function Heading() {
+export default function Hero() {
   return (
     <div className="mt-10">
       <div className="absolute inset-0 flex justify-center items-center">
@@ -14,7 +14,7 @@ export default function Heading() {
       </div>
       <div className="ml-22 w-[30%] mt-10">
         <p className="text-2xl text-[#A1A1AA]">
-          Not just another ed-tech--we’re a SaaS-powered talent accelerator where you learn by building, ship real AI/ML products, and grow continusaly
+          Not just another ed-tech--we’re a SaaS-powered talent accelerator where you learn by building, ship real AI/ML products, and grow continuously
         </p>
       </div>
       <div className="ml-10">
--- frontend/src/landing/landingpage.tsx
+++ frontend/src/landing/landingpage.tsx
@@ -1,11 +1,11 @@
-import Heading from "./_components/heading";
+import Hero from "./_components/hero";
 import Navbar from "./_components/navbar";
 
 export default function LandingPage() {
   return (
     <div className="min-h-screen bg-[#0F1115] text-white">
       <Navbar />
-      <Heading />
+      <Hero />
     </div>
   );
 };
